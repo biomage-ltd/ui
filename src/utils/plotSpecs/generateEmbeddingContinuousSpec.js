a20881c1d494b3b7b52e6237e256df2b5531514c
stefanbabukov <stefanbabukov98@gmail.com>
2020-11-05 12:03:46 +0000

    efforts

--- src/utils/plotSpecs/generateEmbeddingContinuousSpec.js
+++ src/utils/plotSpecs/generateEmbeddingContinuousSpec.js
@@ -29,6 +29,11 @@ const generateSpec = (config) => {
 
     background: config.toggleInvert,
     padding: 5,
+    signals: [
+      { name: 'chartSize', value: 120 },
+      { name: 'chartPad', value: 20 },
+      { name: 'chartStep', update: 'chartSize + chartPad' },
+    ],
     data: [
       {
         name: 'expression',
@@ -47,8 +52,43 @@ const generateSpec = (config) => {
           },
         ],
       },
+      {
+        name: 'fields',
+        values: [
+          'plot1',
+          'plot2',
+        ],
+      },
+      {
+        name: 'cross',
+        source: 'fields',
+        transform: [
+          { type: 'cross', as: ['x', 'y'] },
+          { type: 'formula', as: 'xscale', expr: "datum.x.data + 'X'" },
+          { type: 'formula', as: 'yscale', expr: "datum.y.data + 'Y'" },
+        ],
+      },
     ],
     scales: [
+      {
+        name: 'xall',
+        type: 'band',
+        domain: { data: 'fields', field: 'data' },
+        range: 'width',
+      },
+      {
+        name: 'yall',
+        type: 'band',
+        domain: { data: 'fields', field: 'data' },
+        range: [{ signal: 'height' }, 0],
+      },
+      {
+        name: 'color',
+        type: 'linear',
+        range: { scheme: config.colGradient },
+        domain: { data: 'embedding', field: 'expression' },
+        reverse: config.reverseCbar,
+      },
       {
         name: 'x',
         type: 'linear',
@@ -66,11 +106,20 @@ const generateSpec = (config) => {
         range: 'height',
       },
       {
-        name: 'color',
+        name: 'x2',
         type: 'linear',
-        range: { scheme: config.colGradient },
-        domain: { data: 'embedding', field: 'expression' },
-        reverse: config.reverseCbar,
+        round: true,
+        nice: true,
+        domain: { data: 'embedding', field: '0' },
+        range: 'width',
+      },
+      {
+        name: 'y2',
+        type: 'linear',
+        round: true,
+        nice: true,
+        domain: { data: 'embedding', field: '1' },
+        range: 'height',
       },
     ],
     axes: [
@@ -92,6 +141,8 @@ const generateSpec = (config) => {
         titleColor: { value: config.masterColour },
         labelFontSize: { value: config.axisTicks },
         domainWidth: { value: config.lineWidth },
+        // offset: { signal: '-chartPad' },
+
       },
       {
         scale: 'y',
@@ -112,31 +163,111 @@ const generateSpec = (config) => {
         titleColor: { value: config.masterColour },
         labelFontSize: { value: config.axisTicks },
         domainWidth: { value: config.lineWidth },
+        position: 0,
+
+      },
+      {
+        scale: 'x2',
+        grid: true,
+        domain: true,
+        orient: 'bottom',
+        title: { value: config.xaxisText },
+        titleFont: { value: config.masterFont },
+        labelFont: { value: config.masterFont },
+        labelColor: { value: config.masterColour },
+        tickColor: { value: config.masterColour },
+        gridColor: { value: config.masterColour },
+        gridOpacity: { value: (config.transGrid / 20) },
+        gridWidth: { value: (config.widthGrid / 20) },
+        offset: { value: config.axesOffset },
+        titleFontSize: { value: config.axisTitlesize },
+        titleColor: { value: config.masterColour },
+        labelFontSize: { value: config.axisTicks },
+        domainWidth: { value: config.lineWidth },
+        // offset: { signal: '-chartPad' },
+
+      },
+      {
+        scale: 'y2',
+        grid: true,
+        domain: true,
+        orient: 'left',
+        titlePadding: 5,
+        gridColor: { value: config.masterColour },
+        gridOpacity: { value: (config.transGrid / 20) },
+        gridWidth: { value: (config.widthGrid / 20) },
+        tickColor: { value: config.masterColour },
+        offset: { value: config.axesOffset },
+        title: { value: config.yaxisText },
+        titleFont: { value: config.masterFont },
+        labelFont: { value: config.masterFont },
+        labelColor: { value: config.masterColour },
+        titleFontSize: { value: config.axisTitlesize },
+        titleColor: { value: config.masterColour },
+        labelFontSize: { value: config.axisTicks },
+        domainWidth: { value: config.lineWidth },
+        // position: { signal: '2 * chartStep' },
+
       },
     ],
     marks: [
       {
-        type: 'symbol',
-        from: { data: 'embedding' },
+        type: 'rect',
         encode: {
           enter: {
-            x: { scale: 'x', field: '0' },
-            y: { scale: 'y', field: '1' },
-            size: { value: config.pointSize },
-            stroke: {
-              scale: 'color',
-              field: 'expression',
-            },
-            fill: {
-              scale: 'color',
-              field: 'expression',
-            },
-            shape: { value: config.pointStyle },
-            fillOpacity: { value: config.pointOpa / 10 },
+            fill: { value: '#eee' },
           },
         },
       },
 
+      {
+        name: 'cell',
+        type: 'group',
+        from: { data: 'cross' },
+
+        encode: {
+          enter: {
+            x: { scale: 'xall', field: 'x.data' },
+            y: { scale: 'yall', field: 'y.data' },
+            width: { signal: 'chartSize' },
+            height: { signal: 'chartSize' },
+            fill: { value: 'transparent' },
+            stroke: { value: '#ddd' },
+          },
+        },
+        marks: [
+          {
+            type: 'symbol',
+            from: { data: 'embedding' },
+            encode: {
+              enter: {
+                x: { scale: 'x', field: '0' },
+                y: { scale: 'y', field: '1' },
+                size: { value: config.pointSize },
+                stroke: {
+                  scale: 'color',
+                  field: 'expression',
+                },
+                fill: {
+                  scale: 'color',
+                  field: 'expression',
+                },
+                shape: { value: config.pointStyle },
+                fillOpacity: { value: config.pointOpa / 10 },
+              },
+            },
+          },
+        ],
+      },
+      {
+        type: 'rect',
+        name: 'brush',
+        encode: {
+          enter: {
+            fill: { value: 'transparent' },
+          },
+        },
+      },
     ],
     legends: legend,
     title:
